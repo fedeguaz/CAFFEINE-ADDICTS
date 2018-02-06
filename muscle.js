2f36a375c7ba24a33b1912767f8c0c4da00c8293
fedeguaz <32312506+fedeguaz@users.noreply.github.com>
2018-02-06 23:02:50 +0100

    Update muscle.js

--- muscle.js
+++ muscle.js
@@ -307,7 +307,7 @@ function nuovaPallina() {
 function mouseClicked() {
   dE = dist(mouseX, mouseY, xE, yE);
     if (dE < 40) {
-  window.location.href = "../index.html";
+  window.location.href = "index.html";
     }
 }
 
